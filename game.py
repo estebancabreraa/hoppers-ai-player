c7e93ab58f3142260ac75acd23fd7acb34990ab5
estebancabreraa <esteban.cabrera.a@gmail.com>
2021-06-03 01:29:25 -0600

    main parcialmente terminado

--- game.py
+++ game.py
@@ -1,10 +1,26 @@
 class Game:
 
-    def __init__(self, players, board, turn):
+    def __init__(self, players):
         self.players = players
-        self.board = board
-        self.turn = turn
+        self.board = [[1,1,1,1,1,0,0,0,0,0],
+                    [1,1,1,1,0,2,0,0,0,0],
+                    [1,1,1,0,0,0,0,0,0,0],
+                    [1,1,0,0,0,0,0,0,0,0],
+                    [1,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,2],
+                    [0,0,0,0,0,0,0,0,2,2],
+                    [0,0,0,0,0,0,0,2,2,2],
+                    [0,0,0,0,0,0,2,2,2,2],
+                    [0,0,0,0,0,2,2,2,2,2]]
+        self.turn = 1
+        self.over = True
+    
+    def start(self):
         self.over = False
+
+    def progress(self):
+        while not self.over:
+            i = 10
 
     
 
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-matrix = [[0, 0], [0, 0]]
+from game import *
+from player import *
 
-try:
-    print(matrix[0][0])
-except:
-    print('nel')
+player1 = Player(1, True)
+player2 = Player(2, False)
+
+players = [player1, player2]
+
+game = Game(players)
+
+game.start()
